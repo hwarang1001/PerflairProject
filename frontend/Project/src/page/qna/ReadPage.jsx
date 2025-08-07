859c5f1dadec9cf6afd88f3f00d143368cb0ddbe
Gihuni <huni971209@naver.com>
2025-08-07 10:43:08 +0900

    [Create] Qna List, Read, Write page Create
    * [Create] Qna List, Read, Write page Create
    
    * Revert "[Create] Qna List, Read, Write page Create"
    
    This reverts commit 3840ab2f265218f8e27f8771bfe9c04f9ff98ada.
    
    * [Create] Qna List, Read, Write page Create
    
    (cherry picked from commit 3840ab2f265218f8e27f8771bfe9c04f9ff98ada)

---
+++ frontend/Project/src/page/qna/ReadPage.jsx
@@ -0,0 +1,14 @@
+import ReadComponent from "../../component/qna/ReadComponent";
+import Footer from "../../include/Footer";
+import Header from "../../include/Header";
+
+const ReadPage = () => {
+  return (
+    <>
+      <Header />
+      <ReadComponent />
+      <Footer />
+    </>
+  );
+};
+export default ReadPage;
