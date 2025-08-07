3840ab2f265218f8e27f8771bfe9c04f9ff98ada
Cha Gihun <huni971209@naver.com>
2025-08-07 10:16:17 +0900

    [Create] Qna List, Read, Write page Create

---
+++ frontend/Project/src/page/qna/WritePage.jsx
@@ -0,0 +1,14 @@
+import WriteComponent from "../../component/qna/WriteComponent";
+import Footer from "../../include/Footer";
+import Header from "../../include/Header";
+
+const WritePage = () => {
+  return (
+    <>
+      <Header />
+      <WriteComponent />
+      <Footer />
+    </>
+  );
+};
+export default WritePage;
