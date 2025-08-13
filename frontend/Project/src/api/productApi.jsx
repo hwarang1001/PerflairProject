a1aeda181211e5344a899ccd7873dfab5dea9928
HwaRang Kim <khrang54@gmail.com>
2025-08-13 17:07:47 +0900

    [Update] product ui update

--- frontend/Project/src/api/productApi.jsx
+++ frontend/Project/src/api/productApi.jsx
@@ -1,17 +1,19 @@
 import axios from "axios";
-import jwtAxios from "../util/jwtUtil";
+
 //서버 주소
 export const API_SERVER_HOST = "http://localhost:8080";
 const prefix = `${API_SERVER_HOST}/api/product`;
 
-const apiClient = axios.create({
-  baseURL: `${API_SERVER_HOST}/api/product`,
-  withCredentials: true, // 쿠키나 인증 헤더 포함해서 요청
-});
 export const postAdd = async (product) => {
   //파일업로드 할때에는 기본값인  ‘Content-Type’: ‘application/json’을 ‘multipart/form-data’ 변경해야됨
   const header = { headers: { "Content-Type": "multipart/form-data" } };
-  const res = await jwtAxios.post(`${prefix}/`, product, header);
+  const res = await axios.post(`${prefix}/`, product, header);
+  return res.data;
+};
+
+export const putModify = async (pno, product) => {
+  const header = { headers: { "Content-Type": "multipart/form-data" } };
+  const res = await axios.put(`${prefix}/${pno}`, product, header);
   return res.data;
 };
 
@@ -25,5 +27,10 @@ export const getList = async (pageParam) => {
   const res = await axios.get(`${prefix}/list`, {
     params: { page: page, size: size },
   });
+  return res.data;
+};
+
+export const deleteRemove = async (pno) => {
+  const res = await axios.delete(`${prefix}/${pno}`);
   return res.data;
 };
--- frontend/Project/src/component/product/AdminListComponent .jsx
+++ frontend/Project/src/component/product/AdminListComponent .jsx
@@ -1,5 +1,4 @@
-import { getList } from "../../api/productApi";
-import { API_SERVER_HOST } from "../../api/productApi";
+import { API_SERVER_HOST, deleteRemove, getList } from "../../api/productApi";
 import { useEffect, useState } from "react";
 import useCustomMove from "../../hook/useCustomMove";
 
@@ -19,15 +18,29 @@ const initState = {
 };
 
 const AdminListComponent = () => {
-  const { page, size } = useCustomMove();
+  const { page, size, moveToAdd, moveToModify } = useCustomMove();
   const [serverData, setServerData] = useState(initState);
-
   useEffect(() => {
     getList({ page, size }).then((data) => {
       console.log(data);
       setServerData(data);
     });
   }, [page, size]);
+
+  const btnClickDelete = async (product) => {
+    // 삭제 할지 여부 체크
+    const confirmDelete = window.confirm(
+      `${product.pname}을(를) 삭제하시겠습니까?`
+    );
+    if (!confirmDelete) return;
+
+    // 해당 상품 삭제
+    await deleteRemove(product.pno);
+
+    // 삭제 후 리스트를 다시 받음
+    const data = await getList({ page, size });
+    setServerData(data);
+  };
 
   return (
     <section className="py-5">
@@ -38,14 +51,25 @@ const AdminListComponent = () => {
         </div>
 
         {/* 상품 리스트 플렉스 컨테이너 */}
-        <div className="d-flex flex-column">
+        <div className="d-flex flex-column m-auto w-100">
+          {/* 상품 추가 버튼 - 오른쪽 정렬 */}
+          <div className="d-flex justify-content-end mb-4">
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => {
+                moveToAdd();
+              }}
+            >
+              상품 추가
+            </button>
+          </div>
           {serverData.dtoList.map((product) => (
             <div
-              className="d-flex flex-column flex-md-row align-items-center mb-4 p-3 border rounded justify-content-between"
+              className="d-flex flex-column flex-md-row align-items-center mb-4 p-2 border rounded justify-content-between"
               key={product.pno}
             >
               {/* 상품 이미지 */}
-              <div className="d-flex justify-content-center mb-3 mb-md-0">
+              <div className="d-flex justify-content-center ">
                 <img
                   src={
                     product.uploadFileNames &&
@@ -56,21 +80,33 @@ const AdminListComponent = () => {
                   alt={product.pname}
                   className="img-fluid rounded"
                   style={{
-                    width: "100px",
-                    height: "100px",
+                    width: "70px",
+                    height: "70px",
                     objectFit: "cover",
                   }}
                 />
               </div>
-
-              <h5>{product.pname}</h5>
-              <p>{product.price.toLocaleString()}원</p>
-              <a
-                className="btn btn-outline-dark"
-                href={`/product/read/${product.pno}`}
-              >
-                보러가기
-              </a>
+              <div className="d-flex align-items-center  w-100 m-auto justify-content-center  ">
+                <h5 className="w-25 text-center m-0">{product.brand}</h5>
+                <h5 className="w-25 text-center m-0">{product.pname}</h5>
+                <h5 className="w-25 text-center m-0 ">
+                  {product.options[0].price.toLocaleString()}원
+                </h5>
+              </div>
+              <div className="d-flex gap-2 justify-content-center align-content-center w-25">
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => moveToModify(product.pno)}
+                >
+                  수정
+                </button>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => btnClickDelete(product)}
+                >
+                  삭제
+                </button>
+              </div>
             </div>
           ))}
 
