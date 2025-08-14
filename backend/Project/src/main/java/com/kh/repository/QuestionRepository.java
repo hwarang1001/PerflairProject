c5514f365d2ed87ddabe48d046c9c52de137fcb5
Park Chanhee <1231hero@naver.com>
2025-08-14 11:02:17 +0900

    [Update] question update

--- backend/Project/src/main/java/com/kh/repository/QuestionRepository.java
+++ backend/Project/src/main/java/com/kh/repository/QuestionRepository.java
@@ -1,8 +1,12 @@
 package com.kh.repository;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kh.domain.Question;
 import com.kh.domain.QuestionStatus;
@@ -10,5 +14,15 @@ import com.kh.domain.QuestionStatus;
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
 	
-	 List<Question> findAllByStatusNot(QuestionStatus status);
+	 
+	@Query("SELECT q FROM Question q WHERE q.questionId = :questionId AND q.status <> :status")
+	Optional<Question> findByQuestionIdAndStatusNot(
+	        @Param("questionId") Long questionId,
+	        @Param("status") QuestionStatus status
+	);
+	 
+	// 삭제되지 않은(delFlag = false) 공지사항 목록을 페이징 처리하여 조회
+		// JPQL을 사용하여 Notice 엔티티를 직접 조회합니다.
+	 @Query("SELECT q FROM Question q WHERE q.status <> :status")
+	 Page<Question> findAllNotDeleted(@Param("status") QuestionStatus status, Pageable pageable);
 }
