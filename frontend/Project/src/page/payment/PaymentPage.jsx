398b68753df988b00058cc62a455b85b683c840d
HoJun Lee <tdudal@naver.com>
2025-08-14 17:23:08 +0900

    [Create] payment create

---
+++ frontend/Project/src/page/payment/PaymentPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../include/Header";
+import Footer from "../../include/Footer";
+import PaymentComponent from "../../component/payment/PaymentComponent.jsx";
+
+export default function PaymentPage() {
+  return (
+    <>
+      <Header />
+      <PaymentComponent />
+      <Footer />
+    </>
+  );
+}
