40515a8dd136829f9ef05a08ffa79d76bb8a6eaf
Cha Gihun <huni971209@naver.com>
2025-08-20 18:28:47 +0900

    [Update] login & register update

---
+++ frontend/Project/src/page/login/LoginPage.jsx
@@ -0,0 +1,14 @@
+import LoginComponent from "../../component/login/LoginComponent";
+import Footer from "../../include/Footer";
+import Header from "../../include/Header";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header />
+      <LoginComponent />
+      <Footer />
+    </>
+  );
+};
+export default LoginPage;
